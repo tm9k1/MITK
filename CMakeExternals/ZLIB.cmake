2ffa23fe2272842a84a6a6f3303b57a7b19387a5
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2024-07-05 10:20:28 +0200

    Substitute zlib v1.2.13 with zlib-ng v2.2.1

--- CMakeExternals/ZLIB.cmake
+++ CMakeExternals/ZLIB.cmake
@@ -12,17 +12,17 @@ if(MITK_USE_${proj})
 
   if(NOT DEFINED ${proj}_DIR AND NOT ZLIB_FOUND)
     ExternalProject_Add(${proj}
-      GIT_REPOSITORY https://github.com/madler/zlib.git
-      GIT_TAG v1.2.13
-      CMAKE_GENERATOR ${gen}
-      CMAKE_GENERATOR_PLATFORM ${gen_platform}
+      GIT_REPOSITORY https://github.com/zlib-ng/zlib-ng.git
+      GIT_TAG d54e3769be0c522015b784eca2af258b1c026107 # 2.2.1 (2024-07-02)
       CMAKE_ARGS ${ep_common_args}
+        -DZLIB_COMPAT:BOOL=ON
+        -DZLIB_ENABLE_TESTS:BOOL=OFF
       CMAKE_CACHE_ARGS ${ep_common_cache_args}
       CMAKE_CACHE_DEFAULT_ARGS ${ep_common_cache_default_args}
       DEPENDS ${proj_DEPENDENCIES}
     )
 
-    set(${proj}_DIR ${ep_prefix})
+    set(${proj}_DIR "${ep_prefix}/lib/cmake/ZLIB")
   else()
     mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
   endif()
